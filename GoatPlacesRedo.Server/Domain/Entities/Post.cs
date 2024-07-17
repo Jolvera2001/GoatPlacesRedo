f7dddee6b7caadae77547627f32f049420017938
Jolvera2001 <jolvera6@stedwards.edu>
2024-07-16 22:24:30 -0500

    adding in entities

---
+++ GoatPlacesRedo.Server/Domain/Entities/Post.cs
@@ -0,0 +1,8 @@
+﻿namespace GoatPlacesRedo.Server.Domain.Entities;
+
+public class Post
+{
+    public string Id { get; set; }
+    public User UserId { get; set; }
+    public string body { get; set; }
+}
